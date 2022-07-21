d932171fa1b7ea9e755080461719cff62832856e
尉 <1369727090@qq.com>
2022-07-21 10:01:12 +0800

    update

--- Captainzw/sql.sql
+++ Captainzw/sql.sql
@@ -114,3 +114,20 @@ CREATE TABLE `orders_detail`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
+
+INSERT INTO goods(id,name,introduction,img,price) VALUES(1,'Sukin HandWash洗手液 250ml','消毒 呵护双手','images/hot_1.jpg',47.00);
+INSERT INTO goods(id,name,introduction,img,price)  VALUES(2,'Sukin Conditioner护发素 250ml','天然蛋白滋养','images/hot_2.jpg',40.00);
+INSERT INTO goods(id,name,introduction,img,price)  VALUES(3,'Sukin Shampoo洗发水 250ml','呵护头皮 补水保湿','images/hot_3.jpg',40.00);
+INSERT INTO goods(id,name,introduction,img,price)  VALUES(4,'Sukin RoseHipOil玫瑰果油 25ml','美白淡斑 延缓衰老','images/hot_4.jpg',112.00);
+INSERT INTO goods(id,name,introduction,img,price)  VALUES(5,'Sukin 极致修复滋养润唇膏 10ml','深层修复滋养 保护唇部','images/hot_5.jpg',35.00);
+INSERT INTO goods(id,name,introduction,img,price)  VALUES(6,'Sukin FacialMoisturiser润肤霜 125ml','修复肌肤 滋养保湿','images/hot_6.jpg',45.00);
+INSERT INTO goods(id,name,introduction,img,price) VALUES(7,'Sukin NightCream晚霜 120ml','天然保湿 锁水修复','images/hot_7.jpg',78.00);
+INSERT INTO goods(id,name,introduction,img,price) VALUES(8,'Sukin EyeSerum眼霜 30ml','天然有机 抗氧化','images/hot_8.jpg',77.00);
+INSERT INTO goods(id,name,introduction,img,price) VALUES(9,'Sukin DayCream日霜 120ml','补水保湿 滋润淡斑','images/hot_9.jpg',85.00);
+INSERT INTO goods(id,name,introduction,img,price) VALUES(10,'紧致肌肤 清洁毛孔','Sukin Facial Masque 面膜 100ml','images/hot_10.jpg',65.00);
+INSERT INTO goods(id,name,introduction,img,price) VALUES(11,'Sukin BodyLotion润肤乳液 250ml','天然有机 保湿补水','images/hot_11.jpg',53.00);
+INSERT INTO goods(id,name,introduction,img,price) VALUES(12,'Sukin FacialScrab面部磨砂膏 125ml','去除角质 靓丽肌肤','images/hot_12.jpg',40.00);
+INSERT INTO goods(id,name,introduction,img,price) VALUES(13,'Sukin FoamingCleanser洁面泡沫 125ml','深层清洁 平衡油脂','images/hot_13.jpg',40.00);
+INSERT INTO goods(id,name,introduction,img,price) VALUES(14,'Sukin AvoeVeraGEL芦荟胶 125ml','淡化痘印 美白祛斑','images/hot_14.jpg',40.00);
+INSERT INTO goods(id,name,introduction,img,price) VALUES(15,'Sukin MistToner保湿喷雾 125ml','保湿补水 舒缓修复','images/hot_15.jpg',40.00);
+INSERT INTO goods(id,name,introduction,img,price) VALUES(16,'Sukin BodyWash沐浴露 500ml','舒缓、镇静肌肤','images/hot_16.jpg',203.00);
